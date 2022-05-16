54366e6f74b9840e6f16aa5b675290e084bd604d
julia kharlamova <juliettkhar@gmail.com>
2022-05-16 23:10:20 +0300

    added layout

---
+++ src/components/home/filterForm/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-form :model="formData" label-position="top" class="filter-form">
+    <el-form-item label="Название дороги">
+      <el-input v-model="formData.roadName"></el-input>
+    </el-form-item>
+    <el-form-item label="РГН">
+      <el-input v-model="formData.rgn"></el-input>
+    </el-form-item>
+    <el-form-item label="Городской округ">
+      <el-select
+        v-model="formData.district"
+        placeholder="Выберите из списка"
+        class="filter-form__select">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'FilterForm',
+  setup() {
+    const formData = reactive({
+      roadName: '',
+      rgn: '',
+      district: '',
+    });
+
+    return {
+      formData,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.filter-form {
+
+  &__select {
+    width: 100%;
+  }
+}
+::v-deep .el-form-item__label {
+  width: 100%;
+  line-height: 20px;
+}
+</style>
---
+++ src/components/layout/header/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-header class="app-header" height="auto">
+    <div class="app-header__avatar-wrapper">
+      <el-avatar :size="70"
+                 src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
+      <div class="app-header__title-wrapper">
+        <h1>Название компании</h1>
+        <p>Слоган компании</p>
+      </div>
+    </div>
+    <div class="app-header__login-btn">
+      <h3>Ф.И.О.</h3>
+      <el-button icon="el-icon-user"/>
+    </div>
+  </el-header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped lang="scss">
+.app-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 6px;
+
+  &__title-wrapper {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+
+    h1 {
+      margin: 0 0 10px;
+      line-height: 12px;
+      font-size: 18px;
+      text-align: start;
+
+      @media (max-width: 992px) {
+        font-size: 15px;
+        word-break: break-word;
+      }
+    }
+
+    p {
+      margin: 0;
+
+      @media (max-width: 992px) {
+        display: none;
+      }
+    }
+  }
+
+  &__avatar-wrapper {
+    display: flex;
+    gap: 14px;
+    align-items: center;
+  }
+
+  &__login-btn {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+  }
+}
+</style>
