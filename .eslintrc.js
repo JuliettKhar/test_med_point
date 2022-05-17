e71bc61049a44010edb069cc5e69237987152b38
julia kharlamova <juliettkhar@gmail.com>
2022-05-17 18:28:39 +0300

    configuration

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+    "prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    "vue/attributes-order": [
+      "error",
+      {
+        "order": [
+          "CONDITIONALS",
+          "LIST_RENDERING",
+          "TWO_WAY_BINDING",
+          "RENDER_MODIFIERS",
+          "GLOBAL",
+          [
+            "UNIQUE",
+            "SLOT"
+          ],
+          "OTHER_DIRECTIVES",
+          "DEFINITION",
+          "OTHER_ATTR",
+          "CONTENT",
+          "EVENTS"
+        ],
+        "alphabetical": false
+      }
+    ],
+    "vue/order-in-components": [
+      "error",
+      {
+        "order": [
+          "name",
+          "delimiters",
+          "components",
+          "directives",
+          "filters",
+          "extends",
+          "mixins",
+          "provide",
+          "inject",
+          "inheritAttrs",
+          [
+            "props",
+            "propsData"
+          ],
+          "emits",
+          "setup",
+          "asyncData",
+          "data",
+          "model",
+          "computed",
+          "watch",
+          "watchQuery",
+          "LIFECYCLE_HOOKS",
+          "methods",
+          [
+            "template",
+            "render"
+          ],
+          "renderError"
+        ]
+      }
+    ],
+  },
+};
