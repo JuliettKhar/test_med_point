6ef35c329983ea1cdf58e9789aacca7146581b1f
julia kharlamova <juliettkhar@gmail.com>
2022-05-18 16:46:12 +0300

    update README, small fixes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 
 import '@/plugins/element';
 
@@ -12,6 +11,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
