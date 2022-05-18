62cdb7cb70775b6a6669ef8457e103e546e54bb5
julia kharlamova <juliettkhar@gmail.com>
2022-05-18 12:31:13 +0300

    added axios interceptor

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,9 +1,26 @@
 import Axios from 'axios';
-
-console.log(process.env.VUE_APP_HOST_TO_API);
+import qs from 'qs'
 
 const axios = Axios.create({
   baseURL: process.env.VUE_APP_HOST_TO_API,
 });
+
+axios.interceptors.request.use(
+  (config) => {
+    config.paramsSerializer = (params) =>
+      qs.stringify(params, {
+        skipNulls: true,
+        arrayFormat: 'brackets',
+      });
+
+    return config;
+  },
+
+  (error) => Promise.reject(error),
+);
+
+axios.interceptors.response.use(
+  (response) =>  response.data,
+  (error) => Promise.reject(error.message));
 
 export default axios;
